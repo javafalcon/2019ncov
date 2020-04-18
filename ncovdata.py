db99327dd272808dd37d91d27538fac1d91d8017
javafalcon <javafalcon@163.com>
2020-04-18 20:45:31 +0800

    4-18

--- ncovdata.py
+++ ncovdata.py
@@ -11,44 +11,33 @@ from sklearn.utils import shuffle
 
 def read_seq_data(randompair=False):
     if randompair:
-        negdata = pandas.read_csv('Datasets/negative_random_pair_human_TRB.csv')
-        datafile = 'seqdata_randompair.npz'
+        negdata = pandas.read_csv(r'Datasets/cdhit_data/TRB/neg_random_pair_human_TRB.csv')
+        datafile = 'seqdata_randompair_TRB.npz'
     else:
-        negdata = pandas.read_csv('Datasets/negative_random_CDR3m_human_TRB.csv')
-        datafile = 'seqdata.npz'
-    neg_CDR3_s = negdata.CDR3_s
-    neg_CDR3_m = negdata.CDR3_m
-    neg_CDR3_e = negdata.CDR3_e
+        negdata = pandas.read_csv(r'Datasets/cdhit_data/TRB/neg_random_CDR3m_human_TRB.csv')
+        datafile = 'seqdata_TRB.npz'
+    neg_CDR3 = negdata.CDR3
     neg_Epitope = negdata.Epitope
-    
     negseq=[]
-    for i in range(len(neg_CDR3_s)):
-        s = "" if pandas.isnull( neg_CDR3_s[i]) else neg_CDR3_s[i]
-        m = "" if pandas.isnull( neg_CDR3_m[i]) else neg_CDR3_m[i]
-        e = "" if pandas.isnull( neg_CDR3_e[i]) else neg_CDR3_e[i]
-        negseq.append( s + m + e + neg_Epitope[i])
+    for i in range(len(neg_CDR3)):
+        negseq.append(  neg_CDR3[i] + neg_Epitope[i])
     
-    posdata = pandas.read_csv('Datasets/positive_human_TRB.csv')
-    pos_CDR3_s = posdata.CDR3_s
-    pos_CDR3_m = posdata.CDR3_m
-    pos_CDR3_e = posdata.CDR3_e
+    posdata = pandas.read_csv(r'Datasets/cdhit_data/TRB/pos_human_TRB.csv')
+    pos_CDR3 = posdata.CDR3
     pos_Epitope = posdata.Epitope
     
     posseq=[]
-    for i in range(len(pos_CDR3_s)):
-        s = "" if pandas.isnull( pos_CDR3_s[i]) else pos_CDR3_s[i]
-        m = "" if pandas.isnull( pos_CDR3_m[i]) else pos_CDR3_m[i]
-        e = "" if pandas.isnull( pos_CDR3_e[i]) else pos_CDR3_e[i]
-        posseq.append( s + m + e + pos_Epitope[i])
+    for i in range(len(pos_CDR3)):
+        posseq.append( pos_CDR3[i] + pos_Epitope[i])
     
     np.savez( datafile, negseq=negseq, posseq=posseq)
 
 def read_TRAseq_data(randompair=False):
     if randompair:
-        negdata = pandas.read_csv('Datasets/data_3_9/TRA/neg_random_pair_human_TRA.csv')
+        negdata = pandas.read_csv(r'Datasets/cdhit_data/TRA/neg_random_pair_human_TRA.csv')
         datafile = 'seqdata_randompair_TRA.npz'
     else:
-        negdata = pandas.read_csv('Datasets/data_3_9/TRA/neg_random_CDR3m_human_TRA.csv')
+        negdata = pandas.read_csv(r'Datasets/cdhit_data/TRA/neg_random_CDR3m_human_TRA.csv')
         datafile = 'seqdata_TRA.npz'
     neg_CDR3 = negdata.CDR3
     neg_Epitope = negdata.Epitope
@@ -56,7 +45,7 @@ def read_TRAseq_data(randompair=False):
     for i in range(len(neg_CDR3)):
         negseq.append(  neg_CDR3[i] + neg_Epitope[i])
     
-    posdata = pandas.read_csv('Datasets/data_3_9/TRA/pos_human_TRA.csv')
+    posdata = pandas.read_csv(r'Datasets/cdhit_data/TRA/pos_human_TRA.csv')
     pos_CDR3 = posdata.CDR3
     pos_Epitope = posdata.Epitope
     
@@ -88,8 +77,11 @@ def onehot(seq, row=60, col=20):
     x = np.zeros(shape=(row, col))
     n = len(seq) if len(seq)<=row else row
     for i in range(n):
-        k = amino_acids.index(seq[i])
-        x[i,k] = 1
+        try:
+            k = amino_acids.index(seq[i])
+            x[i,k] = 1
+        except:
+            continue
     return x
 
 def splitTrainAndTest(x, y, test_size=0.2, random_state=None):
@@ -135,18 +127,26 @@ def load_data_onehot(pytpe='TRB', randompair=False, row=60, col=20):
     
     return( x_train, y_train), (x_test, y_test)
     
-def load_data(ptype='TRB'):
+def load_data(ptype='TRB', stype=2):
     if ptype in ['TRB']:
-        data1 = np.load('seqdata_randompair.npz')
-        data2 = np.load('seqdata.npz')
+        data1 = np.load('seqdata_randompair_TRB.npz')
+        data2 = np.load('seqdata_TRB.npz')
     else:
         data1 = np.load('seqdata_randompair_TRA.npz')
         data2 = np.load('seqdata_TRA.npz')
         
-    negseq = np.concatenate((data1['negseq'], data2['negseq']))
+    if stype == 0:
+        negseq = data2['negseq']
+        posseq = data2['posseq']
+    elif stype == 1:
+        negseq = data1['negseq']
+        posseq = data1['posseq']
+    elif stype == 2:
+        negseq = np.concatenate((data1['negseq'], data2['negseq']))
+        posseq = data1['posseq']
     
     x_pos = []
-    for seq in data1['posseq']:
+    for seq in posseq:
         x_pos.append(onehot(seq))
     y_pos = np.zeros(shape=(len(x_pos),2))
     y_pos[:,0] = 1
@@ -174,7 +174,25 @@ def load_data(ptype='TRB'):
     
     return (x_train, y_train), (x_test, y_test)
     
-        
+def load_independ_test(csvfile, row=['CDR3', 'Peptide']):
+    data = pandas.read_csv(csvfile, encoding='gb18030')
+    d1 = data[row[0]] # CDR3
+    d2 = data[row[1]] # peptide
+    x = []
+    for (s1, s2) in zip(d1, d2):
+        x.append(onehot(s1+s2))
+    return np.array(x)   
+
+def load_data_withlabel(csvfile):
+    import keras
+    data = pandas.read_csv(csvfile, encoding='gb18030')
+    cdr3, pept = data['CDR3'], data['Epitope']      
+    y = data['label']
+    x = []
+    for i in range(len(cdr3)):
+        x.append( onehot( cdr3[i] + pept[i]))
+    y = keras.utils.to_categorical(y, num_classes=2)
+    return np.array(x), y
 if __name__ == "__main__":
     """
     data = load_seq_data()   
@@ -195,9 +213,9 @@ if __name__ == "__main__":
     print("In posdata, the max len is: {}, the min len is: {}".format( maxlen2, minlen2))
     """
     #(x_train, y_train), (x_test, y_test) = load_data_onehot()     
-    read_TRAseq_data(randompair=True)
-    read_TRAseq_data(randompair=False)        
+    #read_seq_data(randompair=True)
+    #read_seq_data(randompair=False)        
     #load_data('TRA')
-    
+    x,y = load_data_withlabel('Datasets/pos_training.csv')
     
     
--- predictor.py
+++ predictor.py
@@ -5,18 +5,19 @@ Created on Sat Mar  7 11:04:15 2020
 
 @author: weizhong
 """
-from ncovdata import load_data_onehot, load_data
+from ncovdata import load_data_onehot, load_data, load_independ_test, load_data_withlabel
 import numpy as np
 import os
 from keras import callbacks
 import argparse   
 from keras import layers, models, optimizers
 from keras import backend as K
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from capsulelayers import CapsuleLayer, PrimaryCap, Length, Mask
 from sklearn.utils import class_weight, shuffle, resample
 
-
+tf.disable_eager_execution()
 def CapsNet(input_shape, n_class, num_routing, kernel_size=7):
     """
     A Capsule Network on 2019ncon.
@@ -28,7 +29,7 @@ def CapsNet(input_shape, n_class, num_routing, kernel_size=7):
     x = layers.Input(shape=input_shape)
 
     # Layer 1: Just a conventional Conv2D layer
-    conv1 = layers.Conv2D(filters=128, kernel_size=kernel_size, strides=1, padding='valid', activation='relu', name='conv1')(x)
+    conv1 = layers.Conv2D(filters=128, kernel_size=kernel_size, strides=1, padding='same', activation='relu', name='conv1')(x)
     # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_vector]
     primarycaps = PrimaryCap(conv1, dim_vector=16, n_channels=32, kernel_size=kernel_size, strides=2, padding='valid')
 
@@ -91,15 +92,22 @@ def train(model, data, args):
     
     
     # Training without data augmentation:
-    
-    model.fit([x_train, y_train], [y_train, x_train], 
+    if len(x_test) == 0:
+        model.fit([x_train, y_train], [y_train, x_train], 
+              batch_size=args.batch_size, 
+              epochs=args.epochs,
+              validation_split=0.2, 
+              callbacks=[log, tb, checkpoint, lr_decay])
+    else:
+        model.fit([x_train, y_train], [y_train, x_train], 
               batch_size=args.batch_size, 
               epochs=args.epochs,
               validation_data=[[x_test, y_test], [y_test, x_test]], 
               callbacks=[log, tb, checkpoint, lr_decay])
     
-    model.save_weights(args.save_dir + '/trained_model.h5')
-    print('Trained model saved to \'%s/trained_model.h5\'' % args.save_dir)
+    weights_file = os.path.join(args.save_dir,args.save_weight)
+    model.save_weights(weights_file)
+    print('Trained model saved to {}'.format( weights_file))
 
     from utils import plot_log
     plot_log(args.save_dir + '/log.csv', show=True)
@@ -118,13 +126,16 @@ def writeMetrics(metricsFile, y_true, predicted_Probability, noteInfo=''):
     from sklearn.metrics import accuracy_score, matthews_corrcoef, confusion_matrix
     from sklearn.metrics import f1_score,roc_auc_score,recall_score,precision_score
 
-    predicts = np.array(predicted_Probability> 0.5).astype(int)
-    predicts = predicts[:,0]
-    labels = y_true[:,0]
+    #predicts = np.array(predicted_Probability> 0.5).astype(int)
+    #predicts = predicts[:,0]
+    #labels = y_true[:,0]
+    predicts = np.argmax(predicted_Probability, axis=-1)
+    labels = np.argmax(y_true, axis=-1)
+    
     cm=confusion_matrix(labels,predicts)
     with open(metricsFile,'a') as fw:
         if noteInfo:
-            fw.write(noteInfo + '\n')
+            fw.write('\n\n' + noteInfo + '\n')
         for i in range(2):
             fw.write(str(cm[i,0]) + "\t" +  str(cm[i,1]) + "\n" )
         fw.write("ACC: %f "%accuracy_score(labels,predicts))
@@ -134,8 +145,7 @@ def writeMetrics(metricsFile, y_true, predicted_Probability, noteInfo=''):
         fw.write("\nMCC: %f "%matthews_corrcoef(labels,predicts))
         fw.write("\nAUC: %f\n "%roc_auc_score(labels,predicted_Probability[:,0]))
 
-    
-if __name__ == "__main__":
+def train_main(data, weightFile, info):
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', default=100, type=int)
     parser.add_argument('--epochs', default=15, type=int)
@@ -144,6 +154,7 @@ if __name__ == "__main__":
     parser.add_argument('--shift_fraction', default=0.1, type=float)
     parser.add_argument('--debug', default=0, type=int)  # debug>0 will save weights by TensorBoard
     parser.add_argument('--save_dir', default='./log')
+    parser.add_argument('--save_weight', default=weightFile)
     parser.add_argument('--is_training', default=1, type=int)
     parser.add_argument('--weights', default=None)
     parser.add_argument('--lr', default=0.001, type=float)
@@ -154,25 +165,65 @@ if __name__ == "__main__":
         
     # load data
     #(x_train, y_train), (x_test, y_test) = load_data_onehot(randompair=True)
-    (x_train, y_train), (x_test, y_test) = load_data('TRA')
+    (x_train, y_train), (x_test, y_test) = data
     x_train = x_train.reshape(-1, x_train.shape[1], x_train.shape[2], 1).astype('float32')
-    x_test = x_test.reshape(-1, x_test.shape[1], x_test.shape[2], 1).astype('float32')
+    if len(x_test) > 0:
+        x_test = x_test.reshape(-1, x_test.shape[1], x_test.shape[2], 1).astype('float32')
     
             
     print("predicting x_train.shape:{}".format(x_train.shape))
     # define model
     model = CapsNet(input_shape=x_train.shape[1:],
-                    n_class=len(np.unique(np.argmax(y_train, 1))),
-                    num_routing=args.num_routing, kernel_size=9)
+                    n_class=2,
+                    num_routing=args.num_routing, kernel_size=13)
     model.summary()
     #plot_model(model, to_file=args.save_dir+'/model.png', show_shapes=True)
-
+    
     train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
 
-    #y_pred = test(model=model, data=(x_test, y_test))
-    y_pred, x_recon = model.predict([x_test, y_test], batch_size=100)
+    if len(x_test) > 0:
+        y_pred, x_recon = model.predict([x_test, y_test], batch_size=100)
+        writeMetrics('result.txt', y_test, y_pred, info)
+    else:
+        y_pred, x_recon = model.predict([x_train, y_train], batch_size=100)
+        writeMetrics('result.txt', y_train, y_pred, info)
     K.clear_session()
     tf.reset_default_graph()
-    writeMetrics('result.txt', y_test, y_pred, 'samples rate 1:2 Predicted Metrics:')
+    
+    return model
  
-        
+def independ_test_main(data, model, model_weights, info):
+    #x = load_independ_test(testfile)
+    #y = np.zeros((len(x),2))
+    #if label == 'positive':
+    #    y[:,0] = 1
+    #elif label == 'negative':
+    #    y[:,1] = 1
+    x, y = data
+    x = x.reshape(-1, x.shape[1], x.shape[2], 1).astype('float32')
+    model = CapsNet(input_shape=x.shape[1:], n_class=2, num_routing=5, kernel_size=13)
+    model.load_weights(model_weights)
+    y_pred, x_recon = model.predict([x,y], batch_size=100)
+    writeMetrics('result.txt', y, y_pred, info)
+    
+if __name__ == '__main__':
+    #x_train, y_train = load_data_withlabel('Datasets/pos_training.csv')
+    #indx = np.nonzero(y_train[:,1])
+    #x_train, y_train = x_train[indx]
+    #x_test, y_test = load_data_withlabel('Datasets/pos_testing.csv')
+    #model = train_main(((x_train[:9898], y_train[:9898]),([], [])), './oneclass_trained_model.h5', 'Training in pos_training.csv')
+    #independ_test_main((x_test, y_test), model, './log/trained_model.h5.h5', 'Independ Testing in pos_testing.csv')
+    model = CapsNet(input_shape=(60,20,1), n_class=2, num_routing=5, kernel_size=13)
+    model.load_weights('log/trained_model.h5')
+    x = load_independ_test('Datasets/neg_random_pair_testdata_TRB_hit_VDJDB.csv')
+    x = x.reshape(-1, 60, 20, 1).astype('float32')
+    y_true = np.zeros((len(x),2))
+    y_true[:,0] = 1
+    y_pred, x_recon = model.predict([x, y_true], batch_size=100)
+    yp = np.argmax(y_pred, axis=-1)
+    
+    from sklearn.metrics import accuracy_score, confusion_matrix, matthews_corrcoef
+    print('acc=', accuracy_score(y_true[:,1], yp))
+    
+
+    
