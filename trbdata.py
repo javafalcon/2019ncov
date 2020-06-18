d5390df06bf3666e63ae9a634e32e62fbff4b9ce
javafalcon <javafalcon@163.com>
2020-06-18 15:35:55 +0800

    6/18/2020

--- trbdata.py
+++ trbdata.py
@@ -14,15 +14,21 @@ from sklearn.utils import shuffle
 from sklearn.metrics import accuracy_score, matthews_corrcoef, confusion_matrix
 from sklearn.metrics import f1_score,roc_auc_score,recall_score,precision_score
 
+import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import models
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.regularizers import l2
 from tensorflow.keras.callbacks import LearningRateScheduler, ReduceLROnPlateau, ModelCheckpoint
 
 from resnet import resnet_v1
 from aaindexValues import aaindex1PCAValues
 from Capsule import CapsuleLayer, squash, Length, Mask, margin_loss
+
+trainFile = 'Datasets/TRB/Tran.TCRB.csv'
+testFile = 'Datasets/TRB/Test.TCRB.csv'
+indepFile = 'Datasets/TRB/SARS-CoV-2_TCRB.csv'
 
 def writeMetrics(metricsFile, y_true, predicted_Probability, noteInfo=''):
     predicts = np.argmax(predicted_Probability, axis=-1)
@@ -67,15 +73,13 @@ def read_seqs(file):
     epitope = data.Epitope
     cdr3_seqs, epit_seqs = [], []
     for i in range(len(epitope)):
-        cdr3_seqs.append(cdr3[i][3:-3])
+        cdr3_seqs.append(cdr3[i][2:-1])
         epit_seqs.append(epitope[i])
     
     return cdr3_seqs, epit_seqs, labels
 
 def load_data(col=20, row=9):
-    trainFile = 'Datasets/TRB/Tran.TCRB.csv'
-    testFile = 'Datasets/TRB/Test.TCRB.csv'
-    indepFile = 'Datasets/TRB/SARS-CoV-2_TCRB.csv'
+    
     train_cdr3_seqs,  train_epit_seqs, train_labels = read_seqs(trainFile)
     x_train = np.ndarray(shape=(len(train_cdr3_seqs), row, col+1, 2))
     x_train[:,:,:,0] = pca_code(train_cdr3_seqs, row, col)
@@ -174,8 +178,8 @@ def resnet_train_predict():
     writeMetrics('TRB_result.txt', y_test, y_pred, info)
     
 def capsul_train_predict():
-    (x_train, y_train), (x_test, y_test), (x_indt, y_indt) = load_data(col=9, row=9)
-    model = CapsNet(input_shape=(9,10,2,),kerl=3, n_class=2, num_routing=5)
+    (x_train, y_train), (x_test, y_test), (x_indt, y_indt) = load_data(col=9, row=11)
+    model = CapsNet(input_shape=(11,10,2,),kerl=3, n_class=2, num_routing=5)
     
     model.compile(optimizer=Adam(learning_rate=lr_schedule(0)),
                  loss=[margin_loss, 'mse'],
@@ -210,15 +214,161 @@ def capsul_train_predict():
     indt_pred, _ = model.predict([x_indt, y_indt])
     info = 'By capsul net Tringing in TRB, Testing in nCoV-19...'
     writeMetrics('TRB_result.txt', y_indt, indt_pred, info)
-    
-if  __name__ == "__main__":    
-    capsul_train_predict()
-    #capsul_train_predict()
-        
+
+def resnet_layer(inputs, num_filters, kernel_size=3, strides=1,
+                 activation='relu', batch_normalization=True, conv_first=True):
+    ''' 2D Convolution-Batch Normalization-Activation stack builder
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+
+    # Returns
+        x (tensor): tensor as input to the next layer
     '''
+    conv = layers.Conv2D(num_filters, kernel_size=kernel_size, strides=strides,
+                         padding='same', kernel_initializer='he_normal',
+                         kernel_regularizer=l2(1e-4))
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = layers.BatchNormalization()(x)
+        if activation is not None:
+            x = layers.Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = layers.BatchNormalization()(x)
+        if activation is not None:
+            x = layers.Activation(activation)(x)
+        x = conv(x)
+        
+    return x
+
+def resnet_v1(input_shape, depth, num_classes=2):
+    """ResNet Version 1 Model builder [a]
+
+    Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
+    Last ReLU is after the shortcut connection.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filters is
+    doubled. Within each stage, the layers have the same number filters and the
+    same number of filters.
+    Features maps sizes:
+    stage 0: 32x32, 16
+    stage 1: 16x16, 32
+    stage 2:  8x8,  64
+    The Number of parameters is approx the same as Table 6 of [a]:
+    ResNet20 0.27M
+    ResNet32 0.46M
+    ResNet44 0.66M
+    ResNet56 0.85M
+    ResNet110 1.7M
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth-2)%6 != 0:
+        raise ValueError('depth should be 6n+2')
+    # Start model definition.
+    num_filters = 32
+    num_res_blocks = int((depth-2)/6)
+    
+    inputs = tf.keras.Input(shape=input_shape)
+    x = resnet_layer(inputs, num_filters)
+    # Instantiate teh stack of residual units
+    for stack in range(3):
+        for res_block in range(num_res_blocks):
+            strides = 1
+            if stack > 0 and res_block == 0: # first layer but not first stack
+                strides = 2 # downsample
+            y = resnet_layer(x, num_filters, strides=strides)  
+            y = resnet_layer(y, num_filters, activation=None)
+            
+            if stack > 0 and res_block == 0: # first layer but not first stack
+                # linear projection residual shortcut connection to match
+                # change dims
+                x = resnet_layer(x, num_filters, kernel_size=1, strides=strides,
+                                 activation=None, batch_normalization=False)
+            x = layers.add([x, y])
+            x = layers.Activation('relu')(x)           
+        num_filters *= 2
+        
+    # Add classifier on top.
+    # v1 does not use BN after last shortcut connection-ReLU
+    ax = layers.GlobalAveragePooling2D()(x)
+    #x = layers.AveragePooling2D()(x)
+    
+    ax = layers.Dense(num_filters//8, activation='relu')(ax)
+    ax = layers.Dense(num_filters//2, activation='softmax')(ax)
+    ax = layers.Reshape((1,1,num_filters//2))(ax)
+    ax = layers.Multiply()([ax, x])
+    y = layers.Flatten()(ax)
+    outputs = layers.Dense(num_classes, activation='softmax',
+                           kernel_initializer='he_normal')(y)
+    # Instantiate model
+    model = models.Model(inputs=inputs, outputs=outputs)
+    return model   
+def resnet_attention_train_predict(row, col, modelfile, info):
+    (x_train, y_train), (x_test, y_test), (x_indt, y_indt) = load_data(col=col, row=row)
+    model = resnet_v1(input_shape=(row, col+1, 2), depth=20, num_classes=2)
+    
+    model.compile(optimizer=Adam(learning_rate=lr_schedule(0)),
+                 loss='categorical_crossentropy',
+                 metrics=['accuracy'])
+    
+    lr_scheduler = LearningRateScheduler(lr_schedule)
+    lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0, patience=5,
+                                   min_lr=0.5e-6)
+
+    '''checkpoint = ModelCheckpoint(filepath=modelfile, monitor='val_accuracy',
+                                verbose=1, save_best_only=True,
+                                save_weights_only=True)
+    cbs = [checkpoint, lr_reducer, lr_scheduler]
+    model.fit(x_train, y_train,
+                  batch_size=32,
+                  epochs=10,
+                  validation_data=[x_test, y_test],
+                  shuffle=True,
+                  callbacks=cbs)'''
+    
+    model.load_weights(modelfile)    
+    y_pred = model.predict(x_test)
+    
+    info += 'By resnet with attention Training in TRB, Testing in testdata.....'
+    writeMetrics('TRB_result.txt', y_test, y_pred, info)
+    
+    indt_pred = model.predict(x_indt)
+    info += 'By resnet with attention Tringing in TRB, Testing in nCoV-19...'
+    writeMetrics('TRB_result.txt', y_indt, indt_pred, info)
+
+if  __name__ == "__main__":   
+    '''for row in [9, 12, 17]:
+        for col in [9, 15, 20]:
+            save_dir = os.path.join(os.getcwd(), 'save_models', 'model2')
+            model_name = 'trb_resnet_model_{}_{}.h5'.format(row,col)
+            if not os.path.isdir(save_dir):
+                os.makedirs(save_dir)
+            modelfile = os.path.join(save_dir, model_name) 
+            inf = '-----------------\n'
+            info = 'using {} amino acids of sequence of CDR3, features={}\n'.format(row, col)
+            resnet_attention_train_predict(row, col, modelfile, info)
+            #capsul_train_predict()'''
+    '''    
+    cdr3_seqs, epit_seqs, labels=read_seqs(indepFile)
     # static amino acids frequency
     stat = {}
-    for seq in seqs:
+    for seq in cdr3_seqs:
         stat[len(seq)] = stat.get(len(seq), 0) + 1
    
     items = list(stat.items())
@@ -226,3 +376,6 @@ if  __name__ == "__main__":
     for item in items:
         print("{0:<4}{1:>5}".format(item[0], item[1]))
     '''
+    resnet_attention_train_predict(20,9,'save_models//model1//trb_resnet_model_20_9.h5','Test......')
+    
+        
